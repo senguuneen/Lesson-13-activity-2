2856cc0810f1debc4a615c9bb5e57656f2157096
senguuneen <osenguubee@gmail.com>
2025-04-03 19:18:35 +0800

    commit message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ rows=int(input("please enter the total number of rows: "))
 number=1 
 print("floyds triangle")
 for i in range(1, rows+1):
-    for j in range(i,i+1):
+    for j in range(1,i+1):
         print(number, end ='  ')
         number = number +1
     print()
